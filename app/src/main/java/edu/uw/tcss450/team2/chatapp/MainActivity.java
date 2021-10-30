68435b0945c05949b7e3ed6069ed8cfe5645d7eb
NicolasDeBoer <nicolasdeboer@gmail.com>
2021-10-30 01:58:02 -0700

    Refactor packages scounts>team2 and lab1myfirstapp>chatapp Instances of lab1myfirstapp throughout all changed to some variant of chatapp Replaces starting point lab1 with starting point lab3 auth (Registration and Login)

--- app/src/main/java/edu/uw/tcss450/team2/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/team2/chatapp/MainActivity.java
@@ -1,57 +1,25 @@
 package edu.uw.tcss450.team2.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
-import android.util.Log;
 
+import edu.uw.tcss450.team2.chatapp.model.UserInfoViewModel;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
+
+        new ViewModelProvider(this,
+                new UserInfoViewModel.UserInfoViewModelFactory(args.getEmail(), args.getJwt())
+        ).get(UserInfoViewModel.class);
+
         setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        Log.v("ONSTART", "Custom v message");
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.d("ONRESUME", "Custom d message");
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.i("ONPAUSE", "Custom i message");
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.w("ONSTOP", "Custom w message");
-    }
-
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Log.e("ONDESTROY", "Custom e message");
     }
 }
