e6b1991c9ef2fae508db63a63e444fb888171737
NicolasDeBoer <nicolasdeboer@gmail.com>
2021-11-03 15:48:42 -0700

    Error message for password constraint made specific.

--- app/src/main/java/edu/uw/tcss450/team2/chatapp/ui/auth/register/RegisterFragment.java
+++ app/src/main/java/edu/uw/tcss450/team2/chatapp/ui/auth/register/RegisterFragment.java
@@ -115,7 +115,9 @@ public class RegisterFragment extends Fragment {
         mPassWordValidator.processResult(
                 mPassWordValidator.apply(binding.editPassword1.getText().toString()),
                 this::verifyAuthWithServer,
-                result -> binding.editPassword1.setError("Please enter a password with a special character, no spaces, at least 1 number, at least 1 letter, and at least 7 characters."));
+                result -> binding.editPassword1.setError("Please enter a password with a special" +
+                        " character, no spaces, at least 1 number, at least 1 letter," +
+                        " and at least 7 characters."));
     }
 
     private void verifyAuthWithServer() {
