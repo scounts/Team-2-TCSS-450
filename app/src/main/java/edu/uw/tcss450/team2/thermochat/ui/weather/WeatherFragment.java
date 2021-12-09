899ad06a0982cdcb8ed7fd5fd9afc408b83d7e86
Robbie Talbot <robbie.talbot86@gmail.com>
2021-12-09 13:24:34 -0800

    Home UI and Weather UI changes

--- app/src/main/java/edu/uw/tcss450/team2/thermochat/ui/weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/tcss450/team2/thermochat/ui/weather/WeatherFragment.java
@@ -56,7 +56,7 @@ public class WeatherFragment extends Fragment {
         mModel = new ViewModelProvider(getActivity()).get(WeatherViewModel.class);
         //mModel.
         mModel.addWeatherObserver(getViewLifecycleOwner(), weather ->
-                binding.textviewWeather.setText("" + mModel.getCurrentWeather()));
+                binding.weatherCondition.setText("" + mModel.getCurrentWeather()));
 
     }
 
